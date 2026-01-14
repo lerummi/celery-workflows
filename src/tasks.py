09ea037e7bc98d88a1ae71973a5784069c0e0156
Martin Krause <martin.krause.85@gmx.de>
2026-01-14 15:11:01 +0100

    Cleaning celery tasks

--- src/tasks.py
+++ src/tasks.py
@@ -1,6 +1,8 @@
 from src.celery import celery
-import time
+
 import logging
+from celery import chain
+
 
 @celery.task(bind=True, autoretry_for=(Exception,), retry_backoff=5, retry_kwargs={"max_retries": 3})
 def add(self, x, y):
@@ -8,14 +10,15 @@ def add(self, x, y):
     logging.info(f"add({x}, {y}) = {result}")
     return result
 
+
 @celery.task
 def multiply(x, y):
     result = x * y
     logging.info(f"multiply({x}, {y}) = {result}")
     return result
 
+
 @celery.task
 def workflow_example(a, b):
-    from celery import chain
     workflow = chain(add.s(a, b), multiply.s(10))
     return workflow.apply_async().id
